c9945e04aa5c6770193a15dcb0a1adcff04e792b
atsukiie <veanadoremoz@gmail.com>
2025-11-24 15:03:01 +0800

    Update schema.sql

---
+++ backend/database/schema.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE art_showcase;
+USE art_showcase;
+
+-- 1.) USER TABLE 
+CREATE TABLE users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(20) NOT NULL,
+);
+
+-- 2. ARTWORK TABLE
+CREATE TABLE artwork (
+    artwork_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    title VARCHAR(50),
+    image_url VARCHAR(100),
+
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+-- 3. LIKES TABLE
+CREATE TABLE likes (
+    like_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    artwork_id INT,
+
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (artwork_id) REFERENCES artwork(artwork_id)
+);
